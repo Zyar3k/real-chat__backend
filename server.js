feea0f406a50fec50bf11d55aebf846e57523441
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-20 14:17:03 +0100

    Init project, packages, mongodb

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connect");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on Port ${process.env.PORT}`);
+  console.log(`View: http://localhost:${process.env.PORT}/`);
+});
