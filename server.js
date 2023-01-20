437bd67b6a9730cc6c3fc5be2498159f30859864
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-20 17:56:17 +0100

    Add register funcionality

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 mongoose.set("strictQuery", true);
+
+app.use("/api/auth", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL, {
